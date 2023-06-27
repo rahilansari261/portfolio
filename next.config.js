35c7b68356da0fa437cc5873b6243c970acbb2fd
rahilansari261 <rahilansari261@gmail.com>
2023-06-27 16:16:24 +0530

    domain added in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["media2.giphy.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

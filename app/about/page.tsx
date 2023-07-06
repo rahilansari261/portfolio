977e372ee50a82bcd0693749f180af3ac90eb4a9
rahilansari261 <rahilansari261@gmail.com>
2023-07-06 16:21:35 +0530

    bug fixes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,59 +1,59 @@
 const About = () => {
   return (
-    <div className="flex justify-center;">
-      <div className="max-w-[600px] text-black text-[1.2rem] leading-normal mx-auto my-2.5 px-0 py-5 p-8;">
-        <h2 className="text-[2rem] mb-4;">About Me</h2>
-        <p className="font-light mb-2;">
+    <div className="flex justify-center">
+      <div className="max-w-[600px] text-black text-[1.2rem] leading-normal mx-auto my-2.5 px-0 py-5 p-8">
+        <h2 className="text-[2rem] mb-4">About Me</h2>
+        <p className="font-light mb-2">
           I am a passionate and experienced JavaScript developer with a strong background in creating high-quality web applications. I have experience in a variety of technologies, including React,
           Node.js, and Express. I am highly skilled in front-end and back-end development and have a deep understanding of web development concepts and best practices.
         </p>
-        <p className="font-light mb-2;">
+        <p className="font-light mb-2">
           I am always eager to take on new challenges and am constantly striving to improve my skills and knowledge. I am a quick learner and am able to adapt to new technologies and programming
           languages easily.
         </p>
-        <p className="font-light mb-2;">
+        <p className="font-light mb-2">
           In addition to my technical skills, I am a team player who is able to work well with others and am dedicated to delivering high-quality work on time and on budget. I am also able to
           communicate effectively with clients, stakeholders, and team members.
         </p>
         <div>
-          <h3 className="text-2xl mb-2;">Current Job Title:</h3>
-          <p className="font-light mb-2;">JavaScript Developer</p>
+          <h3 className="text-2xl mb-2">Current Job Title:</h3>
+          <p className="font-light mb-2">JavaScript Developer</p>
         </div>
         <div>
-          <h3 className="text-2xl mb-2;">Education:</h3>
+          <h3 className="text-2xl mb-2">Education:</h3>
           <ul>
-            <li className="font-light mb-2;">Bachelor&apos;s degree in Computer Science from RTU University</li>
+            <li className="font-light mb-2">Bachelor&apos;s degree in Computer Science from RTU University</li>
           </ul>
         </div>
         <div>
-          <h3 className="text-2xl mb-2;">Experience:</h3>
+          <h3 className="text-2xl mb-2">Experience:</h3>
           <ul>
-            <li className="font-light mb-2;">Professional experience in web development</li>
-            <li className="font-light mb-2;">Experience working in Agile development environments</li>
-            <li className="font-light mb-2;">Experience with React, Node.js, Express, and MongoDB</li>
-            <li className="font-light mb-2;">Experience with Git for version control</li>
+            <li className="font-light mb-2">Professional experience in web development</li>
+            <li className="font-light mb-2">Experience working in Agile development environments</li>
+            <li className="font-light mb-2">Experience with React, Node.js, Express, and MongoDB</li>
+            <li className="font-light mb-2">Experience with Git for version control</li>
           </ul>
         </div>
         <div>
-          <h3 className="text-2xl mb-2;">Contact Information:</h3>
+          <h3 className="text-2xl mb-2">Contact Information:</h3>
           <ul>
-            <li className="font-light mb-2;">Email: rahilansari261@gmail.com</li>
-            <li className="font-light mb-2;">Phone: +91 7742148739</li>
-            <li className="font-light mb-2;">
+            <li className="font-light mb-2">Email: rahilansari261@gmail.com</li>
+            <li className="font-light mb-2">Phone: +91 7742148739</li>
+            <li className="font-light mb-2">
               LinkedIn:
-              <a className="text-[#3b5998];" href="https://in.linkedin.com/in/rahil-raza-ansari-34712356">
+              <a className="text-[#3b5998]" href="https://in.linkedin.com/in/rahil-raza-ansari-34712356">
                 linkedin.com/in/rahil-raza-ansari-34712356
               </a>
             </li>
-            <li className="font-light mb-2;">
+            <li className="font-light mb-2">
               Twitter:
-              <a className="text-[#3b5998];" href="https://twitter.com/rahilansari261?t=8VYMXfX8HVQ2XE-7puFoNQ&s=08">
+              <a className="text-[#3b5998]" href="https://twitter.com/rahilansari261?t=8VYMXfX8HVQ2XE-7puFoNQ&s=08">
                 @rahilansari261
               </a>
             </li>
           </ul>
         </div>
-        <p className="font-light mb-2;">
+        <p className="font-light mb-2">
           Please take a look at my portfolio to see some of my recent projects and to get a better sense of my skills and experience. Thank you for visiting my portfolio!
         </p>
       </div>

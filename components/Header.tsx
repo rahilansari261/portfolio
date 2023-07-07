6c36c63c9b49c2a27f6807d2406f2fa562488300
Rahil Ansari <12849916+rahilansari261@users.noreply.github.com>
2023-07-07 12:56:45 +0000

    header bug fixes

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,10 +11,10 @@ const Header = () => {
   };
 
   return (
-    <header>
-      <div className=" w-[300px] flex justify-between items-center;">
+    <header className="flex justify-center md:items-center md:h-[100px] md:w-full">
+      <div className=" w-[300px] flex justify-between items-center md:hidden">
         <Link href="/">
-          <div className="tracking-[2px] leading-[27px] font-[bold] text-4xl text-[#9f9eff];">
+          <div className="tracking-[2px] leading-[27px] font-[bold] text-4xl text-[#9f9eff]">
             <div className=" text-[#433ec4] ">RAHIL</div>
             <div className="text-[#007efc] ">ANSARI</div>
           </div>
@@ -24,39 +24,39 @@ const Header = () => {
           {isDrawerOpen ? <FaTimes /> : <FaBars />}
         </div>
       </div>
-      <nav>
-        <ul
-          className={`fixed bg-black list-none text-white z-[100] ${
-            isDrawerOpen ? "translate-x-0" : "translate-x-full"
-          }  transition-transform duration-[250ms] ease-[cubic-bezier(0.5,0,0.5,1)] h-screen flex flex-col justify-evenly items-center m-0 pt-0 pb-[50px] px-0 inset-0`}
-        >
-          <li className="nav-items">
-            <Link href="/" className="nav__link">
-              Home <span></span>
-            </Link>
-          </li>
-          <li className="nav-items">
-            <Link href="/about" className="nav__link">
-              About <span></span>
-            </Link>
-          </li>
-          <li className="nav-items">
-            <Link href="/projects" className="nav__link">
-              Projects <span></span>
-            </Link>
-          </li>
-          <li className="nav-items">
-            <Link href="/support" className="nav__link">
-              Support <span></span>
-            </Link>
-          </li>
-          <li className="nav-items">
-            <Link href="/contact" className="nav__link">
-              Contact <span></span>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+
+      <ul
+        className={` fixed md:static  bg-black md:bg-transparent md:translate-x-0 list-none text-white z-[100] ${
+          isDrawerOpen ? "translate-x-0" : "translate-x-full"}  transition-transform duration-[250ms] 
+          ease-[cubic-bezier(0.5,0,0.5,1)]  h-screen  md:h-0  flex 
+          flex-col md:flex-row  justify-evenly md:justify-center items-center  inset-0 md:inset-auto `}
+      >
+        <li className="">
+          <Link href="/" className="relative text-[#a09eff] no-underline text-3xl font-black uppercase  md:inline-block  md:text-base md:font-normal  md:mx-2.5 md:my-0 md:pb-[5px]">
+            Home <span className="absolute bg-[#007efc] w-[0%] h-0.5 transition-all duration-[0.6s] left-0 bottom-0 hover:w-full"></span>
+          </Link>
+        </li>
+        <li className="">
+          <Link href="/about" className="relative text-[#a09eff] no-underline text-3xl font-black uppercase  md:inline-block  md:text-base md:font-normal  md:mx-2.5 md:my-0 md:pb-[5px]">
+            About <span className="absolute bg-[#007efc] w-[0%] h-0.5 transition-all duration-[0.6s] left-0 bottom-0 hover:w-full"></span>
+          </Link>
+        </li>
+        <li className="">
+          <Link href="/projects" className="relative text-[#a09eff] no-underline text-3xl font-black uppercase  md:inline-block  md:text-base md:font-normal  md:mx-2.5 md:my-0 md:pb-[5px]">
+            Projects <span className="absolute bg-[#007efc] w-[0%] h-0.5 transition-all duration-[0.6s] left-0 bottom-0 hover:w-full"></span>
+          </Link>
+        </li>
+        <li className="">
+          <Link href="/support" className="relative text-[#a09eff] no-underline text-3xl font-black uppercase  md:inline-block  md:text-base md:font-normal  md:mx-2.5 md:my-0 md:pb-[5px]">
+            Support <span className="absolute bg-[#007efc] w-[0%] h-0.5 transition-all duration-[0.6s] left-0 bottom-0 hover:w-full"></span>
+          </Link>
+        </li>
+        <li className="">
+          <Link href="/contact" className="relative text-[#a09eff] no-underline text-3xl font-black uppercase  md:inline-block  md:text-base md:font-normal  md:mx-2.5 md:my-0 md:pb-[5px]">
+            Contact <span className="absolute bg-[#007efc] w-[0%] h-0.5 transition-all duration-[0.6s] left-0 bottom-0 hover:w-full"></span>
+          </Link>
+        </li>
+      </ul>
     </header>
   );
 };

cecad426e8ebde8c62be86bcd60578481c6d99a9
Rahil Ansari <12849916+rahilansari261@users.noreply.github.com>
2023-07-08 11:11:09 +0000

    support page bug fix

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -1,10 +1,10 @@
 const Support = () => {
   return (
-    <div className="flex flex-col justify-center items-center m-[50px]">
+    <div className="flex flex-col justify-center items-center px-10 py-4 md:max-w-[600px] md:mx-auto">
       <iframe
         id="kofiframe"
         src="https://ko-fi.com/rahilansari/?hidefeed=true&widget=true&embed=true&preview=true"
-        style={{ border: "none", width: "100%", padding: "4px", background: "#f9f9f9", height: "712px" }}
+        style={{ border: "none", width: "100%", padding: "4px", background: "#f9f9f9", height: "720px" }}
       ></iframe>
     </div>
   );

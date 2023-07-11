f209a95b386e3607e5489c1b150b0dd38d50568a
Rahil Ansari <12849916+rahilansari261@users.noreply.github.com>
2023-07-11 11:22:42 +0000

    projects grid done

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,81 +2,29 @@ import Image from "next/image";
 
 const Projects = () => {
   return (
-    <div className="flex justify-center">
-      <div className="text-black p-5">
-        <h2 className="mb-5">My Projects</h2>
-        <div className="mb-5">
-          <h3>Project Name: Tenzies Game</h3>
-          <div className="flex justify-center gap-2.5 mb-[30px]">
-            <div className="basis-6/12">
-              <p>Description: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum, minus, commodi laboriosam cumque.</p>
-
-              <a className="block mt-2.5" href="https://ubiquitous-stroopwafel-a249ed.netlify.app/" target="_blank">
-                Live Demo
-              </a>
-              <a className="block mt-2.5" href="https://github.com/rahilansari261/tenzies" target="_blank">
-                Source Code
-              </a>
-            </div>
-            <div className="basis-2/5">
-              <Image className="h-[200px] max-w-[300px]" src="/assets/images/tenzies.png" alt="tenzies app screenshot" width="480" height="480" />
-            </div>
-          </div>
+    <div className="w-full text-black text-[1.2rem] px-10 py-4 md:max-w-[800px] md:mx-auto">
+      <div className="grid grid-cols-3 ">
+        <div className="border-solid border-2 border-sky-500 relative row-span-2">
+          <Image className="max-w-full max-h-full " src="/assets/images/tree.jpeg" alt="tree-image" fill={true} />
         </div>
-        {/* <div className="project">
-          <h3>Project Name: Tenzies Game</h3>
-          <div className="project-with-image">
-            <div className="project--desc">
-              <p>Description: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum, minus, commodi laboriosam cumque.</p>
-
-              <a href="https://ubiquitous-stroopwafel-a249ed.netlify.app/" target="_blank">
-                Live Demo
-              </a>
-              <a href="https://github.com/rahilansari261/tenzies" target="_blank">
-                Source Code
-              </a>
-            </div>
-            <div className="project--image">
-              <Image className="project--screenshot" src="/assets/images/tenzies.png" alt="tenzies app screenshot" />
-            </div>
-          </div>
-        </div> */}
-        {/* <div className="project">
-          <h3>Project Name: Tenzies Game</h3>
-          <div className="project-with-image">
-            <div className="project--desc">
-              <p>Description: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum, minus, commodi laboriosam cumque.</p>
-
-              <a href="https://ubiquitous-stroopwafel-a249ed.netlify.app/" target="_blank">
-                Live Demo
-              </a>
-              <a href="https://github.com/rahilansari261/tenzies" target="_blank">
-                Source Code
-              </a>
-            </div>
-            <div className="project--image">
-              <Image className="project--screenshot" src="/assets/images/tenzies.png" alt="tenzies app screenshot" />
-            </div>
-          </div>
-        </div> */}
-        {/* <div className="project">
-          <h3>Project Name: Tenzies Game</h3>
-          <div className="project-with-image">
-            <div className="project--desc">
-              <p>Description: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum, minus, commodi laboriosam cumque.</p>
-
-              <a href="https://ubiquitous-stroopwafel-a249ed.netlify.app/" target="_blank">
-                Live Demo
-              </a>
-              <a href="https://github.com/rahilansari261/tenzies" target="_blank">
-                Source Code
-              </a>
-            </div>
-            <div className="project--image">
-              <Image className="project--screenshot" src="/assets/images/tenzies.png" alt="tenzies app screenshot" />
-            </div>
-          </div>
-        </div> */}
+        <div className="border-solid border-2 border-sky-500 relative h-[150px] col-span-2">
+          <Image className="max-w-full max-h-full " src="/assets/images/tree.jpeg" alt="tree-image" fill={true} />
+        </div>
+        <div className="border-solid border-2 border-sky-500 relative h-[150px]">
+          <Image className="max-w-full max-h-full " src="/assets/images/tree.jpeg" alt="tree-image" fill={true} />
+        </div>
+        <div className="border-solid border-2 border-sky-500 relative  row-span-2">
+          <Image className="max-w-full max-h-full " src="/assets/images/tree.jpeg" alt="tree-image" fill={true} />
+        </div>
+        <div className="border-solid border-2 border-sky-500 relative h-[150px] col-span-2">
+          <Image className="max-w-full max-h-full " src="/assets/images/tree.jpeg" alt="tree-image" fill={true} />
+        </div>
+        <div className="border-solid border-2 border-sky-500 relative h-[150px]">
+          <Image className="max-w-full max-h-full " src="/assets/images/tree.jpeg" alt="tree-image" fill={true} />
+        </div>
+        <div className="border-solid border-2 border-sky-500 relative h-[150px] col-span-2">
+          <Image className="max-w-full max-h-full " src="/assets/images/tree.jpeg" alt="tree-image" fill={true} />
+        </div>
       </div>
     </div>
   );

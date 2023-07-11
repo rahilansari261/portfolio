a35d65d7616255ed40e49ba6aafa2d706482f338
Rahil Ansari <12849916+rahilansari261@users.noreply.github.com>
2023-07-11 17:58:24 +0000

    bug fixes

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,27 +3,27 @@ import Image from "next/image";
 const Projects = () => {
   return (
     <div className="w-full text-black text-[1.2rem] px-10 py-4 md:max-w-[800px] md:mx-auto">
-      <div className="grid grid-cols-3 ">
-        <div className="border-solid border-2 border-sky-500 relative row-span-2">
-          <Image className="max-w-full max-h-full " src="/assets/images/tree.jpeg" alt="tree-image" fill={true} />
+      <div className="grid grid-cols-3">
+        <div className="relative row-span-2 ">
+          <Image className="object-cover h-48 " src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
         </div>
-        <div className="border-solid border-2 border-sky-500 relative h-[150px] col-span-2">
-          <Image className="max-w-full max-h-full " src="/assets/images/tree.jpeg" alt="tree-image" fill={true} />
+        <div className="relative col-span-2 ">
+          <Image className="object-cover h-48" src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
         </div>
-        <div className="border-solid border-2 border-sky-500 relative h-[150px]">
-          <Image className="max-w-full max-h-full " src="/assets/images/tree.jpeg" alt="tree-image" fill={true} />
+        <div className="relative">
+          <Image className="object-cover h-48 " src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw"/>
         </div>
-        <div className="border-solid border-2 border-sky-500 relative  row-span-2">
-          <Image className="max-w-full max-h-full " src="/assets/images/tree.jpeg" alt="tree-image" fill={true} />
+        <div className="relative row-span-2">
+          <Image className="object-cover h-48  " src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
         </div>
-        <div className="border-solid border-2 border-sky-500 relative h-[150px] col-span-2">
-          <Image className="max-w-full max-h-full " src="/assets/images/tree.jpeg" alt="tree-image" fill={true} />
+        <div className="relative col-span-2 ">
+          <Image className="object-cover h-48 " src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
         </div>
-        <div className="border-solid border-2 border-sky-500 relative h-[150px]">
-          <Image className="max-w-full max-h-full " src="/assets/images/tree.jpeg" alt="tree-image" fill={true} />
+        <div className="relative ">
+          <Image className="object-cover h-48  " src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
         </div>
-        <div className="border-solid border-2 border-sky-500 relative h-[150px] col-span-2">
-          <Image className="max-w-full max-h-full " src="/assets/images/tree.jpeg" alt="tree-image" fill={true} />
+        <div className="relative col-span-2 ">
+          <Image className="object-cover h-48  " src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
         </div>
       </div>
     </div>

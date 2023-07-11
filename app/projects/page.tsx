8d8777a46c4a10a50349bcb276315404b55e65cd
Rahil Ansari <12849916+rahilansari261@users.noreply.github.com>
2023-07-11 18:00:44 +0000

    bug fixes

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,25 +5,25 @@ const Projects = () => {
     <div className="w-full text-black text-[1.2rem] px-10 py-4 md:max-w-[800px] md:mx-auto">
       <div className="grid grid-cols-3">
         <div className="relative row-span-2 ">
-          <Image className="object-cover h-48 " src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
+          <Image className="object-cover h-48" src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
         </div>
         <div className="relative col-span-2 ">
           <Image className="object-cover h-48" src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
         </div>
         <div className="relative">
-          <Image className="object-cover h-48 " src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw"/>
+          <Image className="object-cover h-48" src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw"/>
         </div>
         <div className="relative row-span-2">
-          <Image className="object-cover h-48  " src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
+          <Image className="object-cover h-48" src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
         </div>
         <div className="relative col-span-2 ">
-          <Image className="object-cover h-48 " src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
+          <Image className="object-cover h-48" src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
         </div>
         <div className="relative ">
-          <Image className="object-cover h-48  " src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
+          <Image className="object-cover h-48" src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
         </div>
         <div className="relative col-span-2 ">
-          <Image className="object-cover h-48  " src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
+          <Image className="object-cover h-48" src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
         </div>
       </div>
     </div>

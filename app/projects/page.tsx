7093562e0f9bb4003ebaa1eecb33a78606402653
Rahil Ansari <12849916+rahilansari261@users.noreply.github.com>
2023-07-12 16:59:31 +0530

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,7 +5,7 @@ const Projects = () => {
     <div className="w-full text-black text-[1.2rem] px-10 py-4 md:max-w-[800px] md:mx-auto">
       <div className="grid grid-cols-3">
         <div className="relative row-span-2 h-[300px]">
-          <Image className="object-cover" src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
+          <Image className="object-cover transition ease-in-out hover:-translate-y-1 duration-1000" src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
         </div>
         <div className="relative col-span-2 h-[150px]">
           <Image className="object-cover" src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />

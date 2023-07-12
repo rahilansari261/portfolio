7f624e3bda0eb2dfc18526f0614ce6406adf3af3
Rahil Ansari <12849916+rahilansari261@users.noreply.github.com>
2023-07-12 17:21:10 +0530

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,7 +5,7 @@ const Projects = () => {
     <div className="w-full text-black text-[1.2rem] px-10 py-4 md:max-w-[800px] md:mx-auto">
       <div className="grid grid-cols-3">
         <div className="relative row-span-2 h-[200px]">
-          <Image className="transition object-cover object-top ease-in-out hover:object-center duration-1000" src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
+          <Image className="transition object-cover object-top ease-in-out transform hover:object-center duration-500" src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
         </div>
         <div className="relative col-span-2 h-[100px]">
           <Image className="transition object-cover object-top ease-in-out hover:object-center duration-1000" src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />

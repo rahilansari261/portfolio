a2cf4d7cd74da197ae5517501b81ae5e99dadf1c
Rahil Ansari <12849916+rahilansari261@users.noreply.github.com>
2023-07-14 11:14:52 +0000

    mobile support added

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,27 +3,27 @@ import Image from "next/image";
 const Projects = () => {
   return (
     <div className="w-full text-black text-[1.2rem] px-10 py-4 md:max-w-[800px] md:mx-auto">
-      <div className="grid grid-cols-3">
-        <div className="relative row-span-2 h-[200px] ">
-          <Image className="object-cover transition-all object-top ease-in-out hover:object-center duration-1000" src="/assets/images/cat.jpg" alt="tree-image" fill sizes="50vw" />
+      <div className="md:grid md:grid-cols-3">
+        <div className="relative row-span-2 h-[300px] ">
+          <Image className="object-cover transition-all object-top ease-in-out hover:object-bottom duration-1000" src="/assets/images/hive.png" alt="tree-image" fill sizes="100vw" />
         </div>
-        <div className="relative col-span-2 h-[100px]">
-          <Image className="object-cover transition-all object-top ease-in-out hover:object-center duration-1000" src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
+        <div className="relative col-span-2 h-[150px]">
+          <Image className="object-cover transition-all object-top ease-in-out hover:object-center duration-1000" src="/assets/images/hive.png" alt="tree-image" fill sizes="100vw" />
         </div>
-        <div className="relative h-[100px]">
-          <Image className="object-cover transition-all object-top ease-in-out hover:object-center duration-1000" src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw"/>
+        <div className="relative h-[150px]">
+          <Image className="object-cover transition-all object-top ease-in-out hover:object-center duration-1000" src="/assets/images/hive.png" alt="tree-image" fill sizes="100vw" />
         </div>
-        <div className="relative row-span-2 h-[200px]">
-          <Image className="object-cover transition-all object-top ease-in-out hover:object-center duration-1000" src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
+        <div className="relative row-span-2 h-[300px]">
+          <Image className="object-cover transition-all object-top ease-in-out hover:object-bottom duration-1000" src="/assets/images/hive.png" alt="tree-image" fill sizes="100vw" />
         </div>
-        <div className="relative col-span-2 h-[100px]">
-          <Image className="object-cover transition-all object-top ease-in-out hover:object-center duration-1000" src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
+        <div className="relative col-span-2 h-[150px]">
+          <Image className="object-cover transition-all object-top ease-in-out hover:object-center duration-1000" src="/assets/images/hive.png" alt="tree-image" fill sizes="100vw" />
         </div>
-        <div className="relative h-[100px]">
-          <Image className="object-cover transition-all object-top ease-in-out hover:object-center duration-1000" src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
+        <div className="relative h-[150px]">
+          <Image className="object-cover transition-all object-top ease-in-out hover:object-center duration-1000" src="/assets/images/hive.png" alt="tree-image" fill sizes="100vw" />
         </div>
-        <div className="relative col-span-2 h-[100px]">
-          <Image className="object-cover transition-all object-top ease-in-out hover:object-center duration-1000" src="/assets/images/cat.jpg" alt="tree-image" fill sizes="100vw" />
+        <div className="relative col-span-2 h-[150px]">
+          <Image className="object-cover transition-all object-top ease-in-out hover:object-center duration-1000" src="/assets/images/hive.png" alt="tree-image" fill sizes="100vw" />
         </div>
       </div>
     </div>

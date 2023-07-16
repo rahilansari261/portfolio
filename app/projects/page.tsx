c8de46f75fe73ba9d0aab297249d1c74f3ef5bbc
Rahil Ansari <12849916+rahilansari261@users.noreply.github.com>
2023-07-16 10:47:20 +0000

    bug fixes

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,155 +3,203 @@ import Link from "next/link";
 
 const Projects = () => {
   return (
-    <div className="w-full text-black text-[1.2rem] px-10 py-4 md:max-w-[800px] md:mx-auto">
-      <div className="md:grid md:grid-cols-3">
-        <div className="relative md:row-span-2 h-[150px] md:h-[300px] group md:bg-black">
+    <div className="w-full text-black text-[1.2rem] px-10 py-4 md:py-10 md:max-w-[800px] md:mx-auto">
+      <div className="md:grid gap-1 md:grid-cols-3 ">
+        <div className="relative md:row-span-2 h-[150px] md:h-[300px] group md:bg-black rounded-lg ">
           <Image
-            className="object-cover transition-all object-top ease-in-out md:group-hover:object-bottom duration-1000 opacity-100 md:group-hover:opacity-40"
+            className="object-cover transition-all object-left-top ease-in-out md:group-hover:object-bottom duration-1000 opacity-100 md:group-hover:opacity-40 rounded-lg outline md:group-hover:outline-4s md:group-hover:outline-indigo-500 "
             src="/assets/images/hive.png"
             alt="tree-image"
             fill
             sizes="100vw"
           />
-          <div className="hidden absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-1000 md:block">
-            Rahil raza Ansari
+          <div className="hidden absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-1000 md:flex flex-col">
+            <div>Rahil</div>
+            <div>Rahil</div>
+            <div>Rahil</div>
           </div>
         </div>
         <div className="flex flex-col py-5 md:hidden">
           <div>Project Name: Rahil</div>
           <div>Tech.: Rahil</div>
           <div>
-            Details: Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos porro assumenda minima, animi dolorum cupiditate debitis vel ea voluptate animi architecto harum pariatur
-            perspiciatis.
+            Details: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Dignissimos porro assumenda minima, animi dolorum cupiditate debitis
+            vel ea voluptate animi architecto harum pariatur perspiciatis.
           </div>
           <Link href="/projects/rahil">
-            <div className=" underline text-blue-900 font-bold ">view more</div>
+            <div className=" hover:underline cursor-pointer text-blue-900 font-bold ">
+              view more
+            </div>
           </Link>
         </div>
-        <div className="relative md:col-span-2 h-[150px] group md:bg-black group">
+        <div className="relative md:col-span-2 h-[150px] group md:bg-black group rounded-lg">
           <Image
-            className="object-cover transition-all object-top ease-in-out group-hover:object-bottom duration-1000 opacity-100 group-hover:opacity-40"
+            className="object-cover transition-all object-top ease-in-out md:group-hover:object-bottom duration-1000 opacity-100 md:group-hover:opacity-40 rounded-lg outline md:group-hover:outline-4s md:group-hover:outline-indigo-500 "
             src="/assets/images/hive.png"
             alt="tree-image"
             fill
             sizes="100vw"
           />
-          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-1000 ">Rahil raza Ansari</div>
+          <div className="hidden absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-1000  md:flex flex-col">
+            <div>Rahil</div>
+            <div>Rahil</div>
+            <div>Rahil</div>
+          </div>
         </div>
         <div className="flex flex-col py-5 md:hidden">
           <div>Project Name: Rahil</div>
           <div>Tech.: Rahil</div>
           <div>
-            Details: Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos porro assumenda minima, animi dolorum cupiditate debitis vel ea voluptate animi architecto harum pariatur
-            perspiciatis.
+            Details: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Dignissimos porro assumenda minima, animi dolorum cupiditate debitis
+            vel ea voluptate animi architecto harum pariatur perspiciatis.
           </div>
           <Link href="/projects/rahil">
-            <div className=" underline text-blue-900 font-bold ">view more</div>
+            <div className=" hover:underline cursor-pointer text-blue-900 font-bold ">
+              view more
+            </div>
           </Link>
         </div>
-        <div className="relative h-[150px] md:bg-black group">
+        <div className="relative h-[150px] md:bg-black group rounded-lg">
           <Image
-            className="object-cover transition-all object-top ease-in-out group-hover:object-bottom duration-1000 opacity-100 group-hover:opacity-40"
+            className="object-cover transition-all object-left-top ease-in-out md:group-hover:object-bottom duration-1000 opacity-100 md:group-hover:opacity-40 rounded-lg outline md:group-hover:outline-4s md:group-hover:outline-indigo-500 "
             src="/assets/images/hive.png"
             alt="tree-image"
             fill
             sizes="100vw"
           />
-          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-1000 ">Rahil raza Ansari</div>
+
+          <div className="hidden absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-1000  md:flex flex-col">
+            <div>Rahil</div>
+            <div>Rahil</div>
+            <div>Rahil</div>
+          </div>
         </div>
         <div className="flex flex-col py-5 md:hidden">
           <div>Project Name: Rahil</div>
           <div>Tech.: Rahil</div>
           <div>
-            Details: Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos porro assumenda minima, animi dolorum cupiditate debitis vel ea voluptate animi architecto harum pariatur
-            perspiciatis.
+            Details: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Dignissimos porro assumenda minima, animi dolorum cupiditate debitis
+            vel ea voluptate animi architecto harum pariatur perspiciatis.
           </div>
           <Link href="/projects/rahil">
-            <div className=" underline text-blue-900 font-bold ">view more</div>
+            <div className=" hover:underline cursor-pointer text-blue-900 font-bold ">
+              view more
+            </div>
           </Link>
         </div>
-        <div className="relative md:row-span-2 h-[150px] md:h-[300px] md:bg-black group">
+        <div className="relative md:row-span-2 h-[150px] md:h-[300px] md:bg-black group rounded-lg">
           <Image
-            className="object-cover transition-all object-top ease-in-out group-hover:object-bottom duration-1000 opacity-100 group-hover:opacity-40"
+            className="object-cover transition-all object-left-top ease-in-out md:group-hover:object-bottom duration-1000 opacity-100 md:group-hover:opacity-40 rounded-lg outline md:group-hover:outline-4s md:group-hover:outline-indigo-500 "
             src="/assets/images/hive.png"
             alt="tree-image"
             fill
             sizes="100vw"
           />
-          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-1000 ">Rahil raza Ansari</div>
+          <div className="hidden absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-1000  md:flex flex-col">
+            <div>Rahil</div>
+            <div>Rahil</div>
+            <div>Rahil</div>
+          </div>
         </div>
         <div className="flex flex-col py-5 md:hidden">
           <div>Project Name: Rahil</div>
           <div>Tech.: Rahil</div>
           <div>
-            Details: Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos porro assumenda minima, animi dolorum cupiditate debitis vel ea voluptate animi architecto harum pariatur
-            perspiciatis.
+            Details: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Dignissimos porro assumenda minima, animi dolorum cupiditate debitis
+            vel ea voluptate animi architecto harum pariatur perspiciatis.
           </div>
           <Link href="/projects/rahil">
-            <div className=" underline text-blue-900 font-bold ">view more</div>
+            <div className=" hover:underline cursor-pointer text-blue-900 font-bold ">
+              view more
+            </div>
           </Link>
         </div>
-        <div className="relative md:col-span-2 h-[150px] md:bg-black group">
+        <div className="relative md:col-span-2 h-[150px] md:bg-black group rounded-lg">
           <Image
-            className="object-cover transition-all object-top ease-in-out group-hover:object-bottom duration-1000 opacity-100 group-hover:opacity-40"
+            className="object-cover transition-all object-top ease-in-out md:group-hover:object-bottom duration-1000 opacity-100 md:group-hover:opacity-40 rounded-lg outline md:group-hover:outline-4s md:group-hover:outline-indigo-500 "
             src="/assets/images/hive.png"
             alt="tree-image"
             fill
             sizes="100vw"
           />
-          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-1000 ">Rahil raza Ansari</div>
+          <div className="hidden absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-1000  md:flex flex-col">
+            <div>Rahil</div>
+            <div>Rahil</div>
+            <div>Rahil</div>
+          </div>
         </div>
         <div className="flex flex-col py-5 md:hidden">
           <div>Project Name: Rahil</div>
           <div>Tech.: Rahil</div>
           <div>
-            Details: Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos porro assumenda minima, animi dolorum cupiditate debitis vel ea voluptate animi architecto harum pariatur
-            perspiciatis.
+            Details: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Dignissimos porro assumenda minima, animi dolorum cupiditate debitis
+            vel ea voluptate animi architecto harum pariatur perspiciatis.
           </div>
           <Link href="/projects/rahil">
-            <div className=" underline text-blue-900 font-bold ">view more</div>
+            <div className=" hover:underline cursor-pointer text-blue-900 font-bold ">
+              view more
+            </div>
           </Link>
         </div>
-        <div className="relative h-[150px]  md:bg-black group">
+        <div className="relative h-[150px]  md:bg-black group rounded-lg">
           <Image
-            className="object-cover transition-all object-top ease-in-out group-hover:object-bottom duration-1000 opacity-100 group-hover:opacity-40"
+            className="object-cover transition-all object-left-top ease-in-out md:group-hover:object-bottom duration-1000 opacity-100 md:group-hover:opacity-40 rounded-lg outline md:group-hover:outline-4s md:group-hover:outline-indigo-500 "
             src="/assets/images/hive.png"
             alt="tree-image"
             fill
             sizes="100vw"
           />
-          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-1000 ">Rahil raza Ansari</div>
+          <div className="hidden absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-1000  md:flex flex-col">
+            <div>Rahil</div>
+            <div>Rahil</div>
+            <div>Rahil</div>
+          </div>
         </div>
         <div className="flex flex-col py-5 md:hidden">
           <div>Project Name: Rahil</div>
           <div>Tech.: Rahil</div>
           <div>
-            Details: Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos porro assumenda minima, animi dolorum cupiditate debitis vel ea voluptate animi architecto harum pariatur
-            perspiciatis.
+            Details: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Dignissimos porro assumenda minima, animi dolorum cupiditate debitis
+            vel ea voluptate animi architecto harum pariatur perspiciatis.
           </div>
           <Link href="/projects/rahil">
-            <div className=" underline text-blue-900 font-bold ">view more</div>
+            <div className="hover:underline cursor-pointer text-blue-900 font-bold ">
+              view more
+            </div>
           </Link>
         </div>
-        <div className="relative md:col-span-2 h-[150px] md:bg-black group">
+        <div className="relative md:col-span-2 h-[150px] md:bg-black group rounded-lg">
           <Image
-            className="object-cover transition-all object-top ease-in-out group-hover:object-bottom duration-1000 opacity-100 group-hover:opacity-40"
+            className="object-cover transition-all object-top ease-in-out md:group-hover:object-bottom duration-1000 opacity-100 md:group-hover:opacity-40 rounded-lg outline md:group-hover:outline-4s md:group-hover:outline-indigo-500 "
             src="/assets/images/hive.png"
             alt="tree-image"
             fill
             sizes="100vw"
           />
-          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-1000 ">Rahil raza Ansari</div>
+          <div className="hidden absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-1000  md:flex flex-col">
+            <div>Rahil</div>
+            <div>Rahil</div>
+            <div>Rahil</div>
+          </div>
         </div>
         <div className="flex flex-col py-5 md:hidden">
           <div>Project Name: Rahil</div>
           <div>Tech.: Rahil</div>
           <div>
-            Details: Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos porro assumenda minima, animi dolorum cupiditate debitis vel ea voluptate animi architecto harum pariatur
-            perspiciatis.
+            Details: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Dignissimos porro assumenda minima, animi dolorum cupiditate debitis
+            vel ea voluptate animi architecto harum pariatur perspiciatis.
           </div>
           <Link href="/projects/rahil">
-            <div className=" underline text-blue-900 font-bold ">view more</div>
+            <div className=" hover:underline cursor-pointer text-blue-900 font-bold ">
+              view more
+            </div>
           </Link>
         </div>
       </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -20,40 +20,65 @@ const Header = () => {
           </div>
         </Link>
 
-        <div className="text-[40px] z-[1000] text-[#433ec4]" aria-label="toggle navigation" onClick={handleNav}>
+        <div
+          className="text-[40px] z-[1000] text-[#433ec4]"
+          aria-label="toggle navigation"
+          onClick={handleNav}
+        >
           {isDrawerOpen ? <FaTimes /> : <FaBars />}
         </div>
       </div>
 
       <ul
         className={` fixed md:static  bg-black md:bg-transparent md:translate-x-0 list-none text-white z-[100] ${
-          isDrawerOpen ? "translate-x-0" : "translate-x-full"}  transition-transform duration-[250ms] 
+          isDrawerOpen ? "translate-x-0" : "translate-x-full"
+        }  transition-transform duration-[250ms] 
           ease-[cubic-bezier(0.5,0,0.5,1)]  h-screen  md:h-0  flex 
           flex-col md:flex-row  justify-evenly md:justify-center items-center  inset-0 md:inset-auto `}
       >
         <li className="" onClick={handleNav}>
-          <Link href="/" className="relative text-[#a09eff] no-underline text-3xl font-black uppercase  md:inline-block  md:text-base md:font-normal  md:mx-2.5 md:my-0 md:pb-[5px]">
-            Home <span className="absolute bg-[#007efc] w-[0%] h-0.5 transition-all duration-[0.6s] left-0 bottom-0 hover:w-full"></span>
+          <Link
+            href="/"
+            className="relative text-[#a09eff] no-underline text-3xl font-black uppercase  md:inline-block  md:text-base md:font-normal  md:mx-2.5 md:my-0 md:pb-[5px]"
+          >
+            Home{" "}
+            <span className="absolute bg-[#007efc] w-[0%] h-0.5 transition-all duration-[0.6s] left-0 bottom-0 hover:w-full"></span>
           </Link>
         </li>
         <li className="" onClick={handleNav}>
-          <Link href="/about" className="relative text-[#a09eff] no-underline text-3xl font-black uppercase  md:inline-block  md:text-base md:font-normal  md:mx-2.5 md:my-0 md:pb-[5px]">
-            About <span className="absolute bg-[#007efc] w-[0%] h-0.5 transition-all duration-[0.6s] left-0 bottom-0 hover:w-full"></span>
+          <Link
+            href="/about"
+            className="relative text-[#a09eff] no-underline text-3xl font-black uppercase  md:inline-block  md:text-base md:font-normal  md:mx-2.5 md:my-0 md:pb-[5px]"
+          >
+            About{" "}
+            <span className="absolute bg-[#007efc] w-[0%] h-0.5 transition-all duration-[0.6s] left-0 bottom-0 hover:w-full"></span>
           </Link>
         </li>
         <li className="" onClick={handleNav}>
-          <Link href="/projects" className="relative text-[#a09eff] no-underline text-3xl font-black uppercase  md:inline-block  md:text-base md:font-normal  md:mx-2.5 md:my-0 md:pb-[5px]">
-            Projects <span className="absolute bg-[#007efc] w-[0%] h-0.5 transition-all duration-[0.6s] left-0 bottom-0 hover:w-full"></span>
+          <Link
+            href="/projects"
+            className="relative text-[#a09eff] no-underline text-3xl font-black uppercase  md:inline-block  md:text-base md:font-normal  md:mx-2.5 md:my-0 md:pb-[5px]"
+          >
+            Projects{" "}
+            <span className="absolute bg-[#007efc] w-[0%] h-0.5 transition-all duration-[0.6s] left-0 bottom-0 hover:w-full"></span>
           </Link>
         </li>
         <li className="" onClick={handleNav}>
-          <Link href="/support" className="relative text-[#a09eff] no-underline text-3xl font-black uppercase  md:inline-block  md:text-base md:font-normal  md:mx-2.5 md:my-0 md:pb-[5px]">
-            Support <span className="absolute bg-[#007efc] w-[0%] h-0.5 transition-all duration-[0.6s] left-0 bottom-0 hover:w-full"></span>
+          <Link
+            href="/support"
+            className="relative text-[#a09eff] no-underline text-3xl font-black uppercase  md:inline-block  md:text-base md:font-normal  md:mx-2.5 md:my-0 md:pb-[5px]"
+          >
+            Support{" "}
+            <span className="absolute bg-[#007efc] w-[0%] h-0.5 transition-all duration-[0.6s] left-0 bottom-0 hover:w-full"></span>
           </Link>
         </li>
         <li className="" onClick={handleNav}>
-          <Link href="/contact" className="relative text-[#a09eff] no-underline text-3xl font-black uppercase  md:inline-block  md:text-base md:font-normal  md:mx-2.5 md:my-0 md:pb-[5px]">
-            Contact <span className="absolute bg-[#007efc] w-[0%] h-0.5 transition-all duration-[0.6s] left-0 bottom-0 hover:w-full"></span>
+          <Link
+            href="/contact"
+            className="relative text-[#a09eff] no-underline text-3xl font-black uppercase  md:inline-block  md:text-base md:font-normal  md:mx-2.5 md:my-0 md:pb-[5px]"
+          >
+            Contact{" "}
+            <span className="absolute bg-[#007efc] w-[0%] h-0.5 transition-all duration-[0.6s] left-0 bottom-0 hover:w-full"></span>
           </Link>
         </li>
       </ul>

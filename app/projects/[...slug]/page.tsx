81d050770e8b2edf6de87c9cbc5bf2f20acc7bdf
Rahil Ansari <12849916+rahilansari261@users.noreply.github.com>
2023-07-21 06:16:30 +0000

    bug fixes

--- app/projects/[...slug]/page.tsx
+++ app/projects/[...slug]/page.tsx
@@ -6,17 +6,17 @@ export default function Page({ params }: { params: { slug: string } }) {
     <div className="w-full text-black text-[1.2rem] px-10 py-4 md:py-10 md:max-w-[800px] md:mx-auto">
       <div className="relative md:row-span-2 h-[150px] md:h-[300px] group md:bg-black rounded-lg ">
         <Image
-          className="object-contain rounded-lg outline outline-4s outline-indigo-500"
+          className="object-cover object-top rounded-lg outline outline-4s outline-indigo-500"
           src="/assets/images/1-pw.png"
           alt="tree-image"
           fill
           sizes="100vw"
         />
-        <div className="text-black flex flex-col">
-          <div>Rahil</div>
-          <div>Rahil</div>
-          <div>Rahil</div>
-        </div>
+      </div>
+      <div className=" text-black flex flex-col">
+        <div>Rahil</div>
+        <div>Rahil</div>
+        <div>Rahil</div>
       </div>
     </div>
   );
